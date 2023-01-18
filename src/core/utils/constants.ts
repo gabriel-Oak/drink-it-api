77568eabf7334029f66bab2d21d92007029284bb
gabriel-Oak <gabrielcarvalhocosta@live.com>
2023-01-18 16:38:53 -0300

    cache service

--- src/core/utils/constants.ts
+++ src/core/utils/constants.ts
@@ -8,3 +8,7 @@ export const POSTGRE_USER = process.env.POSTGRE_USER;
 export const POSTGRE_PASS = process.env.POSTGRE_PASS;
 export const POSTGRE_PORT = process.env.POSTGRE_PORT;
 export const POSTGRE_DB = process.env.POSTGRE_DB;
+
+export const REDIS_PORT = process.env.REDIS_PORT;
+export const REDIS_HOST = process.env.REDIS_HOST;
+export const REDIS_PASS = process.env.REDIS_PASS;
